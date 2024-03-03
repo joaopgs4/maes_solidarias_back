77089c0490ac584ff81c4907aef9ff580fb0b954
joaopgs4 <jpgardenalli@gmail.com>
2024-03-03 14:04:10 -0300

    first commit

---
+++ store/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name', 'name')
+        verbose_name_plural = 'Categorias'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="store_products", blank=True, null=True)
+
+    #Foreign keys
+    #Como python não é uma linguagem compilada, para referir a uma classe precisamos chama-la após a criação. Por isso Produto vem depois de Categoria
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE) 
