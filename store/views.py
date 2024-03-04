89426cad608f1980e075b4222894cd2430541e6f
caio <caioliberal@gmail.com>
2024-03-04 17:09:07 -0300

    Algumas URLS implementadas

--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+from .models import Category
+@csrf_exempt
+def categoria(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        categoria = Category(name=nome)
+        categoria.save()
+        return render(request, 'store/add_category.html', {'categoria': categoria})
+    if request.method == 'GET':
+        return 
