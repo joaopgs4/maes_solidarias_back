768b50124cb09ee17ad9402508789516ee47c9e2
joaopgs4 <jpgardenalli@gmail.com>
2024-03-05 12:22:22 -0300

    Fix: merge

--- maes_solidarias_back/urls.py
+++ maes_solidarias_back/urls.py
@@ -21,6 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('bazar/', include('store.urls')),
     path('events/', include('events.urls')),
-
-    path('access/', include('access.urls'))
 ]
